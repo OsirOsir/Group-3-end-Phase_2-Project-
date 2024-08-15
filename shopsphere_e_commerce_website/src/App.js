b08654b8d0e52f6dbd84ed8dab7274b1986fc6a2
OsirDev <philiposir2@gmail.com>
2024-08-15 21:50:47 +0300

    Update App.js

--- shopsphere_e_commerce_website/src/App.js
+++ shopsphere_e_commerce_website/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import ShoesSection from './ShoesSection';
-import ElectronicsSection from './ElectronicsSection';
+
 
 function App() {
   return (
     <div className="App">
      <main>
-        <ShoesSection />
-        <ElectronicsSection />
+        
       </main>
     </div>
   );
