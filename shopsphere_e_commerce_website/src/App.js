c14bc72d81e4b2696bb3586699b51adf6234d810
Philip osir Otieno <philiposir2@gmail.com>
2024-08-15 19:57:56 +0300

    updates

--- shopsphere_e_commerce_website/src/App.js
+++ shopsphere_e_commerce_website/src/App.js
@@ -1,18 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import Categories from './components/Categories';
 import FAQ from './components/FAQ';
 
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <Categories />
       <FAQ />
-      <Footer />
     </div>
   );
 }
