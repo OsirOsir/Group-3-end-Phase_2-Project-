fa92581ea76293c07e5e3c7c2a326182ef8cf003
Philip osir Otieno <philiposir2@gmail.com>
2024-08-16 04:43:01 +0300

    branch

--- shopsphere_e_commerce_website/src/App.js
+++ shopsphere_e_commerce_website/src/App.js
@@ -1,13 +1,8 @@
-
 import React, { useState, useEffect } from 'react';
 import FlashSale from './components/FlashSale';
 import HotInCategory from './components/HotInCategory';
 import Cartlist from './components/CartList';
-import ClothesSection from './components/ClothesSection';
-import WhatsNew from './components/WhatsNew';
 import './App.css';
-
-import Footer from './components/Footer';
 import './components/Cart.css';
 import './components/FlashSale.css';
 import Book from './components/Book';
@@ -15,18 +10,17 @@ import Artwork from './components/Artwork';
 import Cart from './Cart';
 import ShoesSection from './ShoesSection';
 import ElectronicsSection from './ElectronicsSection';
-import FAQ from './components/FAQ';
+
 import Navbar from './components/Navbar'
 import Categories from './components/Categories';
+import Footer from './components/Footer'
+import FAQ from './components/FAQ';
 
 function App() {
 
   const [ flashSaleItems, setFlashSaleItems ] = useState([]);
   const [ hotItems, setHotItems ] = useState([]);
   const [ cart, setCart ] = useState([]);
-  const [products, setProducts] = useState([]);  // Add setProducts here
-  const [showClothes, setShowClothes] = useState(true);
-  const [showWhatsNew, setShowWhatsNew] = useState(false);
   
 
   const addToCart = item => {
@@ -37,11 +31,6 @@ function App() {
   const removeFromCart = itemId => {
     setCart(cart.filter(item => item.id !== itemId));
   };
-  useEffect(() => {
-    fetch('http://localhost:5500/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));  // Use setProducts here
-  }, []);
 
   useEffect(() => {
     fetch("http://localhost:8001/flashSale")
@@ -75,13 +64,7 @@ function App() {
       <FlashSale flashSaleItems={flashSaleItems} cart={cart} onAddToCart={handleAddToCart} onRemoveFromCart={handleRemoveFromCart} />
       <HotInCategory hotItems={hotItems} cart={cart} onAddToCart={handleAddToCart} onRemoveFromCart={handleRemoveFromCart}/>
       <Cartlist cart={cart} onRemoveFromCart={handleRemoveFromCart}/>
-      <nav>
-        <button onClick={() => { setShowClothes(true); setShowWhatsNew(false); }}>Clothes</button>
-        <button onClick={() => { setShowClothes(false); setShowWhatsNew(true); }}>What's New</button>
-      </nav>
-      {showClothes && <ClothesSection />}
-      {showWhatsNew && <WhatsNew />}
-        <div className="containers">
+      <div className="containers">
         <Book addToCart={addToCart} />
         <Artwork addToCart={addToCart} />
       </div>
@@ -94,7 +77,5 @@ function App() {
   );
 }
 
-
 export default App;
-
 
